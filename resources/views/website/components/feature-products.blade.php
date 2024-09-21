c0f48f16f97c2788e41a36c0871efe36b90c0f29
Shahd-Elmnyar <shahdmohamed2632003@gmail.com>
2024-09-21 14:30:41 +0200

    🌍 Enhance Localization in Website Components
    - 🌐 Updated breadcrumb links to use localization for the "Home" text.
    - 🛍️ Translated "Featured Products" heading and product details in `feature-products.blade.php`.
    - 🥳 Localized navbar items including navigation, promo, and contact info.
    - 📧 Improved email subscription form placeholder and submit button text.
    - ©️ Added localized copyright message and template attribution.

--- resources/views/website/components/feature-products.blade.php
+++ resources/views/website/components/feature-products.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-7 site-section-heading text-center pt-4">
-            <h2>Featured Products</h2>
+            <h2>{{ __('products.featured_products') }}</h2>
         </div>
     </div>
     <div class="row">
@@ -10,11 +10,11 @@
                 <div class="item">
                     <div class="block-4 text-center">
                         <figure class="block-4-image">
-                            <img src="{{ asset('assets/images/cloth_1.jpg')}}" alt="Image placeholder" class="img-fluid">
+                            <img src="{{ asset('assets/images/cloth_1.jpg')}}" alt="{{ __('products.image_placeholder') }}" class="img-fluid">
                         </figure>
                         <div class="block-4-text p-4">
-                            <h3><a href="#">Tank Top</a></h3>
-                            <p class="mb-0">Finding perfect t-shirt</p>
+                            <h3><a href="#">{{ __('products.tank_top') }}</a></h3>
+                            <p class="mb-0">{{ __('products.perfect_tshirt') }}</p>
                             <p class="text-primary font-weight-bold">$50</p>
                         </div>
                     </div>
@@ -22,11 +22,11 @@
                 <div class="item">
                     <div class="block-4 text-center">
                         <figure class="block-4-image">
-                            <img src="{{ asset('assets/images/shoe_1.jpg')}}" alt="Image placeholder" class="img-fluid">
+                            <img src="{{ asset('assets/images/shoe_1.jpg')}}" alt="{{ __('products.image_placeholder') }}" class="img-fluid">
                         </figure>
                         <div class="block-4-text p-4">
-                            <h3><a href="#">Corater</a></h3>
-                            <p class="mb-0">Finding perfect products</p>
+                            <h3><a href="#">{{ __('products.corater') }}</a></h3>
+                            <p class="mb-0">{{ __('products.perfect_products') }}</p>
                             <p class="text-primary font-weight-bold">$50</p>
                         </div>
                     </div>
@@ -34,11 +34,11 @@
                 <div class="item">
                     <div class="block-4 text-center">
                         <figure class="block-4-image">
-                            <img src="{{ asset('assets/images/cloth_2.jpg')}}" alt="Image placeholder" class="img-fluid">
+                            <img src="{{ asset('assets/images/cloth_2.jpg')}}" alt="{{ __('products.image_placeholder') }}" class="img-fluid">
                         </figure>
                         <div class="block-4-text p-4">
-                            <h3><a href="#">Polo Shirt</a></h3>
-                            <p class="mb-0">Finding perfect products</p>
+                            <h3><a href="#">{{ __('products.polo_shirt') }}</a></h3>
+                            <p class="mb-0">{{ __('products.perfect_products') }}</p>
                             <p class="text-primary font-weight-bold">$50</p>
                         </div>
                     </div>
@@ -46,11 +46,11 @@
                 <div class="item">
                     <div class="block-4 text-center">
                         <figure class="block-4-image">
-                            <img src="{{ asset('assets/images/cloth_3.jpg')}}" alt="Image placeholder" class="img-fluid">
+                            <img src="{{ asset('assets/images/cloth_3.jpg')}}" alt="{{ __('products.image_placeholder') }}" class="img-fluid">
                         </figure>
                         <div class="block-4-text p-4">
-                            <h3><a href="#">T-Shirt Mockup</a></h3>
-                            <p class="mb-0">Finding perfect products</p>
+                            <h3><a href="#">{{ __('products.tshirt_mockup') }}</a></h3>
+                            <p class="mb-0">{{ __('products.perfect_products') }}</p>
                             <p class="text-primary font-weight-bold">$50</p>
                         </div>
                     </div>
@@ -58,11 +58,11 @@
                 <div class="item">
                     <div class="block-4 text-center">
                         <figure class="block-4-image">
-                            <img src="{{ asset('assets/images/shoe_1.jpg')}}" alt="Image placeholder" class="img-fluid">
+                            <img src="{{ asset('assets/images/shoe_1.jpg')}}" alt="{{ __('products.image_placeholder') }}" class="img-fluid">
                         </figure>
                         <div class="block-4-text p-4">
-                            <h3><a href="#">Corater</a></h3>
-                            <p class="mb-0">Finding perfect products</p>
+                            <h3><a href="#">{{ __('products.corater') }}</a></h3>
+                            <p class="mb-0">{{ __('products.perfect_products') }}</p>
                             <p class="text-primary font-weight-bold">$50</p>
                         </div>
                     </div>
