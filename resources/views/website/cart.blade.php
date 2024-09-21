f1d4788a997b09f1caf6ab4e7264625794efd3dc
Shahd-Elmnyar <shahdmohamed2632003@gmail.com>
2024-09-21 16:30:45 +0200

    🛠️ Add localization for Cart and Contact pages
    - Updated Cart and Contact pages to use localization keys for strings
    - Replaced hardcoded text with translations for better i18n support
    - Enhanced accessibility with localized alt attributes for images and aria labels

--- resources/views/website/cart.blade.php
+++ resources/views/website/cart.blade.php
@@ -1,8 +1,7 @@
 @extends('website.layout')
-@section('title', 'Cart')
+@section('title', 'cart')
 @section('content')
-@include('website.components.breadcrumb', ['pageName' => 'Cart'])
-
+@include('website.components.breadcrumb', ['pageName' => 'cart'])
 
 <div class="site-section">
     <div class="container">
@@ -12,21 +11,21 @@
                     <table class="table table-bordered">
                         <thead>
                             <tr>
-                                <th class="product-thumbnail">Image</th>
-                                <th class="product-name">Product</th>
-                                <th class="product-price">Price</th>
-                                <th class="product-quantity">Quantity</th>
-                                <th class="product-total">Total</th>
-                                <th class="product-remove">Remove</th>
+                                <th class="product-thumbnail">{{ __('cart.image') }}</th>
+                                <th class="product-name">{{ __('cart.product') }}</th>
+                                <th class="product-price">{{ __('cart.price') }}</th>
+                                <th class="product-quantity">{{ __('cart.quantity') }}</th>
+                                <th class="product-total">{{ __('cart.total') }}</th>
+                                <th class="product-remove">{{ __('cart.remove') }}</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td class="product-thumbnail">
-                                    <img src="{{ asset('assets/images/cloth_1.jpg')}}" alt="Image" class="img-fluid">
+                                    <img src="{{ asset('assets/images/cloth_1.jpg')}}" alt="{{ __('cart.image') }}" class="img-fluid">
                                 </td>
                                 <td class="product-name">
-                                    <h2 class="h5 text-black">Top Up T-Shirt</h2>
+                                    <h2 class="h5 text-black">{{ __('cart.top_up_t_shirt') }}</h2>
                                 </td>
                                 <td>$49.00</td>
                                 <td>
@@ -34,7 +33,7 @@
                                         <div class="input-group-prepend">
                                             <button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button>
                                         </div>
-                                        <input type="text" class="form-control text-center" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+                                        <input type="text" class="form-control text-center" value="1" placeholder="" aria-label="{{ __('cart.quantity') }}" aria-describedby="button-addon1">
                                         <div class="input-group-append">
                                             <button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button>
                                         </div>
@@ -43,6 +42,7 @@
                                 </td>
                                 <td>$49.00</td>
                                 <td><a href="#" class="btn btn-primary btn-sm">X</a></td>
+
                             </tr>
 
                             <tr>
@@ -50,7 +50,7 @@
                                     <img src="{{ asset('assets/images/cloth_2.jpg')}}" alt="Image" class="img-fluid">
                                 </td>
                                 <td class="product-name">
-                                    <h2 class="h5 text-black">Polo Shirt</h2>
+                                    <h2 class="h5 text-black">{{__('cart.polo_shirt')}}</h2>
                                 </td>
                                 <td>$49.00</td>
                                 <td>
@@ -78,22 +78,22 @@
             <div class="col-md-6">
                 <div class="row mb-5">
                     <div class="col-md-6 mb-3 mb-md-0">
-                        <button class="btn btn-primary btn-sm btn-block">Update Cart</button>
+                        <button class="btn btn-primary btn-sm btn-block">{{ __('cart.update_cart') }}</button>
                     </div>
                     <div class="col-md-6">
-                        <button class="btn btn-outline-primary btn-sm btn-block">Continue Shopping</button>
+                        <button class="btn btn-outline-primary btn-sm btn-block">{{ __('cart.continue_shopping') }}</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <label class="text-black h4" for="coupon">Coupon</label>
-                        <p>Enter your coupon code if you have one.</p>
+                        <label class="text-black h4" for="coupon">{{ __('cart.coupon') }}</label>
+                        <p>{{ __('cart.coupon_instruction') }}</p>
                     </div>
                     <div class="col-md-8 mb-3 mb-md-0">
-                        <input type="text" class="form-control py-3" id="coupon" placeholder="Coupon Code">
+                        <input type="text" class="form-control py-3" id="coupon" placeholder="{{ __('cart.coupon_code') }}">
                     </div>
                     <div class="col-md-4">
-                        <button class="btn btn-primary btn-sm">Apply Coupon</button>
+                        <button class="btn btn-primary btn-sm">{{ __('cart.apply_coupon') }}</button>
                     </div>
                 </div>
             </div>
@@ -102,12 +102,12 @@
                     <div class="col-md-7">
                         <div class="row">
                             <div class="col-md-12 text-right border-bottom mb-5">
-                                <h3 class="text-black h4 text-uppercase">Cart Totals</h3>
+                                <h3 class="text-black h4 text-uppercase">{{ __('cart.cart_totals') }}</h3>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
-                                <span class="text-black">Subtotal</span>
+                                <span class="text-black">{{ __('cart.subtotal') }}</span>
                             </div>
                             <div class="col-md-6 text-right">
                                 <strong class="text-black">$230.00</strong>
@@ -115,7 +115,7 @@
                         </div>
                         <div class="row mb-5">
                             <div class="col-md-6">
-                                <span class="text-black">Total</span>
+                                <span class="text-black">{{ __('cart.total') }}</span>
                             </div>
                             <div class="col-md-6 text-right">
                                 <strong class="text-black">$230.00</strong>
@@ -124,7 +124,7 @@
 
                         <div class="row">
                             <div class="col-md-12">
-                                <button class="btn btn-primary btn-lg py-3 btn-block" onclick="window.location='checkout.html'">Proceed To Checkout</button>
+                                <button class="btn btn-primary btn-lg py-3 btn-block" onclick="window.location='checkout.html'">{{ __('cart.proceed_to_checkout') }}</button>
                             </div>
                         </div>
                     </div>
